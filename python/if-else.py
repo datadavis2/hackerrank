d8e36a71250b424f11aca526d8453c76a62ef528
Chris Davis <christophergdavis@gmail.com>
2018-02-20 15:16:34 -0600

    Update if-else.py

--- python/if-else.py
+++ python/if-else.py
@@ -3,18 +3,20 @@ Task
 Given an integer, , perform the following conditional actions:
 
 If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird
 Input Format
 
 A single line containing a positive integer, .
 
 Constraints
+1 <= n <= 100
 
 Output Format
-
 Print Weird if the number is weird; otherwise, print Not Weird.
+
+https://www.hackerrank.com/challenges/py-if-else/problem
 '''
 
 n = int(input())
